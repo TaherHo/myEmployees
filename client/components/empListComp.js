583034141df802b0cd286f8c35da964c4793ee6d
taHo <hoseininejad@gmail.com>
2017-08-07 09:41:12 +0200

    Added new Component empDetailsComp.

--- client/components/empListComp.js
+++ client/components/empListComp.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import EmpDetails from './empDetailsComp';
 
 const EmpList = () =>{
     return(
         <div>
-            from empList
+            <EmpDetails />
         </div>
     );
 };
