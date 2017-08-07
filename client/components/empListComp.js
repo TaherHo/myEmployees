1095e737e3225dc67db6070cb0931b0ba76890bc
taHo <hoseininejad@gmail.com>
2017-08-07 15:52:31 +0200

    Changed functional react to class base react model./n Completed 'load more' button.

--- client/components/empListComp.js
+++ client/components/empListComp.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import EmpDetails from './empDetailsComp';
 import {createContainer} from 'meteor/react-meteor-data';
 import {EmpCol} from '../../imports/collections/ourEmployees';
 
 const PER_PAGE = 20;
 
-const EmpList = ({employees}) => {
+class EmpList extends Component {
+    componentWillMount(){
+        this.page= 1;
+    }
 
-    return (
-        <div>
-            <div className="employees-list">
-                {employees.map(employee =>
-                    <EmpDetails key={employee._id} employee={employee}/>
-                )}
+    loadClickHandler(){
+        Meteor.subscribe('empCol', PER_PAGE*(this.page+1));
+        this.page ++;
+    }
+    render() {
+        return (
+            <div>
+                <div className="employees-list">
+                    {this.props.employees.map(employee =>
+                        <EmpDetails key={employee._id} employee={employee}/>
+                    )}
+                </div>
+                <button onClick={this.loadClickHandler.bind(this)} className="btn btn-primary"> Load more</button>
             </div>
-            <button onClick={() => Meteor.subscribe('empCol' , 40)} className="btn btn-primary"> Load more</button>
-        </div>
 
-    );
-};
+        );
+    };
+}
+
 
 export default createContainer(() => {
     Meteor.subscribe('empCol', PER_PAGE);
