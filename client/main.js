5d8b7a7d2da61e789eb4704c2061740a3994b460
taHo <hoseininejad@gmail.com>
2017-08-11 07:52:40 +0200

    Added the primary boulerplate.

---
+++ client/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import reactDOM from "react-dom";
+
+const App = () =>{
+    return(
+        <div>
+            Hi there!
+        </div>);
+};
+
+reactDOM.render( <App/> , document.querySelector('generated-components'));
